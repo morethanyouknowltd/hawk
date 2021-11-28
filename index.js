4cfb4e7349a177d4f9d73253a9d4d3a7d0a47c27
Andy Shand <andy@morethanyouknow.co.uk>
2021-11-28 12:42:57 +0000

    Colorise some output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const chokidar = require("chokidar")
 const { spawn, execSync } = require("child_process")
 var args = process.argv.slice(2)
-
+const chalk = require("chalk")
 let spawned
 let spawning = false
 
@@ -22,31 +22,44 @@ function debounce(func, wait, immediate = false) {
   }
 }
 
-const restartChild = debounce(async () => {
-  if (spawning) {
-    return
-  }
-  spawning = true
-  function spawnNext() {
-    const toSpawn = args.slice(1).join(" ")
-    console.log(`Running "${toSpawn}"`)
-    // execSync(`kill $(lsof -t -i:3000) | exit 0`)
-    // console.log(process.cwd())
-    spawned = spawn(args[1], args.slice(2), {
-      stdio: [0, 1, 2],
-    })
-    spawning = false
-    // console.log(`Child process id is ${spawned.pid}`)
-  }
-  if (spawned) {
-    spawned.on("exit", () => {
+let killedByUs = false
+const restartChild = debounce(
+  async () => {
+    if (spawning) {
+      return
+    }
+    spawning = true
+    function spawnNext() {
+      const toSpawn = args.slice(1).join(" ")
+      console.log(`Running "${toSpawn}"`)
+      // execSync(`kill $(lsof -t -i:3000) | exit 0`)
+      // console.log(process.cwd())
+      spawned = spawn(args[1], args.slice(2), {
+        stdio: [0, 1, 2],
+      })
+      spawned.on("exit", function () {
+        if (!killedByUs) {
+          console.log(chalk.green("Process exited, restart hawk to restart"))
+          process.exit()
+        }
+      })
+      spawning = false
+      killedByUs = false
+      // console.log(`Child process id is ${spawned.pid}`)
+    }
+    if (spawned) {
+      killedByUs = true
+      spawned.on("exit", () => {
+        spawnNext()
+      })
+      spawned.kill()
+    } else {
       spawnNext()
-    })
-    spawned.kill()
-  } else {
-    spawnNext()
-  }
-}, 1000)
+    }
+  },
+  1000,
+  { leading: true, trailing: true }
+)
 
 const toWatch = args[0]
 console.log(`Watching ${toWatch}`)
@@ -56,7 +69,9 @@ chokidar
     ignoreInitial: true,
   })
   .on("all", (event, path) => {
-    console.log(`Restarting because of "${path}"`)
+    console.log(
+      `\n${chalk.green("Restarting because of")} ${chalk.yellow(path)}`
+    )
     restartChild()
   })
 
