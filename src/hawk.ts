6312b6da022b9b214d2d24ee28bbc5a85eddc5cd
Andy Shand <andy@morethanyouknow.co.uk>
2022-06-06 14:00:09 +0100

    Fix bin script not running under node interpreter

--- src/hawk.ts
+++ src/hawk.ts
@@ -1,14 +1,15 @@
-import { Cli } from "clipanion";
+#!/usr/bin/env node
+import { Cli } from 'clipanion'
 
-import { MainCommand } from "./MainCommand";
-import packageJson = require("../package.json");
-const [node, app, ...args] = process.argv;
+import { MainCommand } from './MainCommand'
+import packageJson = require('../package.json')
+const [node, app, ...args] = process.argv
 
 const cli = new Cli({
   binaryLabel: `Hawk`,
   binaryName: `${node} ${app}`,
   binaryVersion: packageJson.version,
-});
+})
 
-cli.register(MainCommand);
-cli.runExit(args);
+cli.register(MainCommand)
+cli.runExit(args)
